25eafdae0725bb2c6640ee998d31c33209bd313e
bharath <33729709+bharath5673@users.noreply.github.com>
2021-08-06 14:18:20 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,53 +2,24 @@ from tracker import Tracker
 from detector import Detector
 import imutils, argparse, cv2
 import os
-from glob import glob
 
 
+cap = cv2.VideoCapture(0)
+# tracker = Tracker(model='yolox-s', ckpt='YOLOX/weights/yolox_s.pth',filter_class=['truck','person','car'])
+tracker = Tracker(model='yolox-s', ckpt='YOLOX/weights/yolox_s.pth',filter_class=['person'])
+# tracker = Tracker()
+while True:
+    _, im = cap.read()
+    if im is None:
+        break
+    im = imutils.resize(im, height=640)
+    image,_ = tracker.update(im)
+   
 
-def track_images(img_dir):
-    tracker = Tracker(model='yolox-s', ckpt='weights/yolox_s.pth.tar',filter_class=['truck','person','car'])
-    imgs = glob(os.path.join(img_dir,'*.png')) + glob(os.path.join(img_dir,'*.jpg')) + glob(os.path.join(img_dir,'*.jpeg'))
-    for path in imgs:
-        im = cv2.imread(path)
-        im = imutils.resize(im, height=400)
-        image,_ = tracker.update(im)
-        image = imutils.resize(image, height=500)
+    cv2.imshow('demo', image)
+    key = cv2.waitKey(1) & 0xFF
 
-        cv2.imshow('demo', image)
-        cv2.waitKey(1)
-        if cv2.getWindowProperty('demo', cv2.WND_PROP_AUTOSIZE) < 1:
-            break
-    cv2.destroyAllWindows()
-
-
-
-def track_cap(file):
-    cap = cv2.VideoCapture(file)
-    tracker = Tracker()
-    while True:
-        _, im = cap.read()
-        if im is None:
-            break
-        im = imutils.resize(im, height=500)
-        image,_ = tracker.update(im)
-       
- 
-        cv2.imshow('demo', image)
-        cv2.waitKey(1)
-        if cv2.getWindowProperty('demo', cv2.WND_PROP_AUTOSIZE) < 1:
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser("YOLOX-Tracker Demo!")
-    parser.add_argument('-p', "--path", type=str, help="choose a video")
-    args = parser.parse_args()
-
-    if os.path.isfile(args.path):
-        track_cap(args.path)
-    else:
-        track_images(args.path)
+    if key == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows() 
